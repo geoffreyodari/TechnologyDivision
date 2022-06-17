26471fe93fb8caa00701be6904cbb0f24247bafb
Geoffrey Odari <geoffrey.ochenge@gmail.com>
2022-06-17 14:44:52 +0300

    created Staff class

---
+++ src/main/java/models/Staff.java
@@ -0,0 +1,16 @@
+package models;
+
+public class Staff {
+    private String staffName;
+    private String role;
+    private String department;
+    private String division;
+
+    public void Staff(String staffName,String role, String department,String division){
+       this.staffName =  staffName;
+       this.role = role;
+       this.department = department;
+       this.division = division;
+    }
+
+}

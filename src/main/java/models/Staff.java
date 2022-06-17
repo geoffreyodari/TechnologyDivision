42ae758f3d3a85b1d0f441aa1e519547e357a711
Geoffrey Odari <geoffrey.ochenge@gmail.com>
2022-06-17 15:24:30 +0300

    tested getters in the Staff class

--- src/main/java/models/Staff.java
+++ src/main/java/models/Staff.java
@@ -6,11 +6,33 @@ public class Staff {
     private String department;
     private String division;
 
-    public void Staff(String staffName,String role, String department,String division){
+    public  Staff(String staffName,String role, String department,String division){
        this.staffName =  staffName;
        this.role = role;
        this.department = department;
        this.division = division;
     }
+
+    public String getName(){
+        //returns the staff name
+        return this.staffName;
+    }
+
+    public String getRole(){
+        //returns the staff Role
+        return this.role;
+    }
+
+    public String getDepartment(){
+        //returns the staff Department
+        return this.department;
+    }
+
+    public String getDivision(){
+        //returns the staff Division
+        return this.division;
+    }
+
+
 
 }
--- src/test/java/models/StaffTest.java
+++ src/test/java/models/StaffTest.java
@@ -7,9 +7,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StaffTest {
     @Test
     public void newStaff_instantiatesCorrectly(){
-        Staff testStaff = new Staff();
+        Staff testStaff = new Staff("james","Quality Analyst","Dev","Technology");
         assertEquals(true, testStaff instanceof Staff);
     }
+
+
+    @Test
+    public void newStaff_getsName() {
+        Staff testStaff = new Staff("james","Quality Analyst","Dev","Technology");
+        assertEquals("james", testStaff.getName());
+    }
+
+    @Test
+    public void newStaff_getsRole() {
+        Staff testStaff = new Staff("james","Quality Analyst","Dev","Technology");
+        assertEquals("Quality Analyst", testStaff.getRole());
+    }
+
+    @Test
+    public void newStaff_getsDivision() {
+        Staff testStaff = new Staff("james","Quality Analyst","Dev","Technology");
+        assertEquals("Technology", testStaff.getDivision());
+    }
+
+
 
 
 }

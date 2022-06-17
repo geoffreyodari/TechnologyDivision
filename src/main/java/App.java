dae1411795c7588c26be1f3e8029f36db8d3e8e5
Geoffrey Odari <geoffrey.ochenge@gmail.com>
2022-06-17 17:03:19 +0300

    added static file location

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,20 +2,16 @@ import models.Staff;
 
 import java.util.Scanner;
 
+import static spark.Spark.*;
+
 public class App {
     public static void main(String[] args) {
-        Scanner inputString = new Scanner(System.in);
-        System.out.println("Please Enter Name");
-        String  name = inputString.next();
-        System.out.println("Please Enter Role");
-        String  role = inputString.next();
-        System.out.println("Please Enter Department");
-        String  department = inputString.next();
-        System.out.println("Please Enter Division");
-        String  division = inputString.next();
+        staticFileLocation("/public");
 
-        Staff staff = new Staff(name,role,department,division);
+        //get("/",(request,response)->"hello");
 
-        System.out.println("Your name is "+ staff.getName()+"\n");
+        //Staff staff = new Staff(name,role,department,division);
+
+
     }
 }
